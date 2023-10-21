87d6bb088a3d7deab0af6f0bae2eedb286f42d12
baezus <elias@baez.us>
2023-10-21 00:47:50 -0400

    laid sass foundation

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className="container is-fluid">
-    <div className="notification is-primary">
+  <div className="container is-fluid is-fullheight">
+    <div className="container background mb-6 mt-6 mx-6">
       <App />
     </div>
   </div>
